76e2dca99b9c2c6ec8d9c6d62e0fe7d58c96c390
‘Andrei <andrei.dima@usm.ro>
2023-01-19 11:42:40 +0200

    a

--- resources/views/statistica/index.blade.php
+++ resources/views/statistica/index.blade.php
@@ -10,7 +10,26 @@
                 <div class="card-body">
                     @include ('errors')
 
-
+                    @php
+                        // $cartiScanate->sortBy('created_at' 'asc')->first();
+                        // dd($cartiScanate->sortBy('created_at')->first()->created_at);
+                        $primulAn = \Carbon\Carbon::parse($cartiScanate->sortBy('created_at')->first()->created_at)->year;
+                    @endphp
+                    @for ($an = $primulAn; $an <= \Carbon\Carbon::today()->year; $an++)
+                        @php
+                            $cartiScanateInAcestAn = $cartiScanate->where('created_at', '>', \Carbon\Carbon::createFromDate($an, 1, 1))
+                                                                        ->where('created_at', '<', \Carbon\Carbon::createFromDate($an, 12, 31));
+                        @endphp
+                        <b>{{ $an }}</b>
+                        <br>
+                        Cărți scanate: {{ $cartiScanateInAcestAn->count() }}
+                        <br>
+                        Pagini scanate: {{ $cartiScanateInAcestAn->sum('nr_pagini') }}
+                        <br><br>
+                    @endfor
+                    <br><br><br>
+                    <b>Toti anii</b>
+                    <br>
                     Total cărți scanate: {{ $cartiScanate->count() }}
                     <br>
                     Total pagini scanate: {{ $cartiScanate->sum('nr_pagini') }}
